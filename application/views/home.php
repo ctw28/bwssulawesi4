a17d90a500ddbf4630417484fc23204b01f79ef4
localhouse <mbandrigo@gmail.com>
2019-08-01 18:14:25 +0800

    newsticker

--- application/views/home.php
+++ application/views/home.php
@@ -9,6 +9,7 @@
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <!-- Style -->
   <link href="<?php echo base_url()?>assets/css/style2.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo base_url()?>assets/plugins/news-ticker/breaking-news-ticker.css">
 </head>
 <body>
     <div class="container">
@@ -131,13 +132,18 @@
 
 
                 <section class="support-section">
-                    <input type="text" name="search">
-                    <div class="pull-right">
-                        <a href="#" class="fa fa-facebook"></a>
-                        <a href="#" class="fa fa-google"></a>
-                        <a href="#" class="fa fa-youtube"></a>
-                        <a href="#" class="fa fa-instagram"></a>
-                        <!--<img src="assets/images/logo/pengaduan.png"> -->
+                    <div class="breaking-news-ticker" id="example" style="height: 20px;">
+                      <div class="bn-label">Kilas Berita</div>
+                      <div class="bn-news">
+                        <ul>
+                          <li><a href="#">Selamat Datang di Website Resmi Balai Wilayah Sungai Sulawesi IV Kendari</a></li>
+                          <li><a href="#">Selamat Datang di Website Resmi Balai Wilayah Sungai Sulawesi IV Kendari</a></li>
+                        </ul>
+                      </div>
+                    </div>
+
+                    <div class="search-container">
+                        <input type="text" name="search">
                     </div>
                 </section>
 
@@ -180,56 +186,55 @@
 
                 <section class="content-part-2">
                     <div class="container-1">
-                        <div class="title-container-content"><i class="fa fa-bullhorn "></i> Pengumuman</div>
-                        <div class="list-item-2">
-                            <ul>
-                                <a href=""><li><i class="fa fa-caret-right"></i> Pemberitahuan Rencana Pengadaan Tanah Bendungan Ladongi</li></a>
-                                <a href=""><li><i class="fa fa-caret-right"></i> Pengadaan Tanah Bendungan Ladongi</li></a>
-                            </ul>
-                        </div>
                         <div class="title-container-content"><i class="fa fa-bullseye "></i> Poling</div>
-                        <div class="poling">
+                        <div class="content-container poling">
                           <p>Apa Pendapat Anda Tentang Tampilan Website ini ?</p>
                           <input type="radio" name="opt" id="opt1" checked> <label for="opt1">Bagus Sekali</label>
                           <input type="radio" name="opt" id="opt2"> <label for="opt2"> Bagus</label>
                           <input type="radio" name="opt" id="opt3"> <label for="opt3"> Biasa</label>
                           <input type="radio" name="opt" id="opt4"> <label for="opt4"> Kurang</label>
                         </div>
+                        <div class="title-container-content"><i class="fa fa-bullhorn "></i> Pengumuman</div>
+                        <div class="list-item-2">
+                            <ul class="content-container ">
+                                <a href=""><li><i class="fa fa-caret-right"></i> Pemberitahuan Rencana Pengadaan Tanah Bendungan Ladongi</li></a>
+                                <a href=""><li><i class="fa fa-caret-right"></i> Pengadaan Tanah Bendungan Ladongi</li></a>
+                            </ul>
+                        </div>
                         <div class="title-container-content"><i class="fa fa-book"></i> Publikasi</div>
-                            <div class="publikasi">
-                                <div class="publikasi-item">
-                                    <img src="assets/images/publikasi.jpg">
-                                    <p>Buku Tahunan</p>
-                                </div>
-                                <div class="publikasi-item">
-                                    <img src="assets/images/publikasi.jpg">
-                                    <p>Buletin</p>
-                                </div>
-                                <div class="publikasi-item">
-                                    <img src="assets/images/publikasi.jpg">
-                                    <p>Leaflet</p>
-                                </div>
+                        <div class="publikasi">
+                            <div class="publikasi-item">
+                                <img src="assets/images/buletin.jpg">
+                                <p>Buletin</p>
                             </div>
-
+                            <div class="publikasi-item">
+                                <img src="assets/images/buletin.jpg">
+                                <p>Buletin</p>
+                            </div>
+                            <div class="publikasi-item">
+                                <img src="assets/images/buletin.jpg">
+                                <p>Buletin</p>
+                            </div>
+                        </div>
                     </div>
                     <div class="container-2">
                         <div class="title-container-content"><i class="fa fa-bookmark "></i> Events</div>
                         <div class="list-item-3">
-                            <div class="event-item">
+                            <div class="content-container event-item">
                                 <h6>Upacara hari Kemerdekaan RI Ke - 74</h6>
                                 <!-- <p>Kepada Seluruh Karyawan/Karywati BWS Sulawesi IV Kendari untuk menghadiri Upacara Peringatan Hari Kemerdekaan RI Ke - 74 : </p> -->
                                 <i class="fa fa-map-marker "></i> Halaman kantor BWS Sulawesi IV Kendari <br>
                                 <i class="fa fa-calendar"></i> Sabtu, 17 Agustus 2019  
                                 <i class="fa fa-clock-o"></i> 07.00 Wita
                             </div>
-                            <div class="event-item">
+                            <div class="content-container event-item">
                                 <h6>Senam Jumat</h6>
                                 <!-- <p>Diharapkan Kepada Seluruh Karyawan/Karywati BWS Sulawesi IV Kendari untuk mengikuti senam jumat : </p> -->
                                 <i class="fa fa-map-marker "></i> Halaman kantor BWS Sulawesi IV Kendari <br>
                                 <i class="fa fa-calendar"></i> Sabtu, 17 Agustus 2019  
                                 <i class="fa fa-clock-o"></i> 07.00 Wita
                             </div>
-                            <div class="event-item">
+                            <div class="content-container event-item">
                                 <h6>Hari Raya Idul Adha 1440 H</h6>
                                 <!-- <p>Kepada Seluruh Karyawan/Karywati BWS Sulawesi IV Kendari untuk mengambil hewan kurban : </p> -->
                                 <i class="fa fa-map-marker "></i> Parkir Belakang BWS Sulawesi IV Kendari <br>
@@ -238,6 +243,9 @@
                             </div>
                         </div>
                         <div class="title-container-content"><i class="fa fa-bookmark "></i> Pengunjung Website</div>
+                            <div class="content-container pengunjung-container">
+                                <h4>999999999999</h4>
+                            </div>
 
                     </div>
                     <div class="container-3">
@@ -247,9 +255,34 @@
                     </div>
                 </section>
 
-                <div class="title-container-content"><i class="fa fa-building "></i> Kegiatan Strategis</div>
-                <section class="kegiatan-strategis">
-                    
+                <div class="title-container-content"><i class="fa fa-building "></i> Kegiatan Strategis
+                    <a class="pull-right" href="#">Lihat Semua <i class="fa fa-angle-double-right "></i></a>
+                </div>
+                <section class="kegiatan-strategis list-container">
+                    <div class="list-item-4">
+                        <img src="assets/images/ladongi.jpg">
+                        <div class="list-item-title-2">
+                            <a href="">Bendungan ladongi</a>
+                            <p>Lokasi : Kabupaten Kolaka Timur<br>
+                            Tahun : 2016-2020</p>
+                        </div>                        
+                    </div>
+                    <div class="list-item-4">
+                        <img src="assets/images/pelosika.jpg">
+                        <div class="list-item-title-2">
+                            <a href="">Bendungan Pelosika</a>
+                            <p>Lokasi : Kabupaten Kolaka Timur dan Konawe<br>
+                            Tahun : 2019-2023</p>
+                        </div>
+                    </div>
+                    <div class="list-item-4">
+                        <img src="assets/images/tongauna.jpg">
+                        <div class="list-item-title-2">
+                            <a href="">Bendung Tongauna</a>
+                            <p>Lokasi : Kabupaten Kolaka Timur<br>
+                            Tahun : 2016-2018</p>
+                        </div>
+                    </div>
                 </section>
 
 
@@ -268,6 +301,20 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="<?php echo base_url()?>assets/plugins/news-ticker/breaking-news-ticker.min.js"></script>
+  <script>
+    $('#example').breakingNews({
+        height: 30,
+        fontSize: 12,
+        themeColor: "default",
+        background: "#002B5C",
+        borderWidth: 1,
+        radius: 10,
+        zIndex: 99999
+    });
+
+
+  </script>
 </body>
 </html>
 
