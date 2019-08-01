612296f0711ea081e1ab954ade1d8487dd3f3918
rigo <mbandrigo@gmail.com>
2019-08-01 08:28:54 +0800

    ubah 2 rumah

--- application/views/home.php
+++ application/views/home.php
@@ -123,7 +123,7 @@
                                 <iframe style="padding-right: 1%;" src="https://www.youtube.com/embed/mmKjbxASGlQ" frameborder="0" allowfullscreen></iframe>
                             </div>
                             <div class="embed-responsive embed-responsive-21by9">
-                                <iframe src="https://www.youtube.com/embed/V6wAyXetXDY" frameborder="0" allowfullscreen></iframe>
+                                <iframe style="padding-left: 1%;" src="https://www.youtube.com/embed/V6wAyXetXDY" frameborder="0" allowfullscreen></iframe>
                             </div>
                         </div>
                     </section>
@@ -187,23 +187,72 @@
                                 <a href=""><li><i class="fa fa-caret-right"></i> Pengadaan Tanah Bendungan Ladongi</li></a>
                             </ul>
                         </div>
+                        <div class="title-container-content"><i class="fa fa-bullseye "></i> Poling</div>
+                        <div class="poling">
+                          <p>Apa Pendapat Anda Tentang Tampilan Website ini ?</p>
+                          <input type="radio" name="opt" id="opt1" checked> <label for="opt1">Bagus Sekali</label>
+                          <input type="radio" name="opt" id="opt2"> <label for="opt2"> Bagus</label>
+                          <input type="radio" name="opt" id="opt3"> <label for="opt3"> Biasa</label>
+                          <input type="radio" name="opt" id="opt4"> <label for="opt4"> Kurang</label>
+                        </div>
+                        <div class="title-container-content"><i class="fa fa-book"></i> Publikasi</div>
+                            <div class="publikasi">
+                                <div class="publikasi-item">
+                                    <img src="assets/images/publikasi.jpg">
+                                    <p>Buku Tahunan</p>
+                                </div>
+                                <div class="publikasi-item">
+                                    <img src="assets/images/publikasi.jpg">
+                                    <p>Buletin</p>
+                                </div>
+                                <div class="publikasi-item">
+                                    <img src="assets/images/publikasi.jpg">
+                                    <p>Leaflet</p>
+                                </div>
+                            </div>
+
                     </div>
                     <div class="container-2">
-                        <div class="title-container-content"><i class="fa fa-bullhorn "></i> Pengumuman</div>
-                        <div class="list-item-2">
-                            <ul>
-                                <a href=""><li><i class="fa fa-caret-right"></i> Pemberitahuan Rencana Pengadaan Tanah Bendungan Ladongi</li></a>
-                                <a href=""><li><i class="fa fa-caret-right"></i> Pengadaan Tanah Bendungan Ladongi</li></a>
-                            </ul>
+                        <div class="title-container-content"><i class="fa fa-bookmark "></i> Events</div>
+                        <div class="list-item-3">
+                            <div class="event-item">
+                                <h6>Upacara hari Kemerdekaan RI Ke - 74</h6>
+                                <!-- <p>Kepada Seluruh Karyawan/Karywati BWS Sulawesi IV Kendari untuk menghadiri Upacara Peringatan Hari Kemerdekaan RI Ke - 74 : </p> -->
+                                <i class="fa fa-map-marker "></i> Halaman kantor BWS Sulawesi IV Kendari <br>
+                                <i class="fa fa-calendar"></i> Sabtu, 17 Agustus 2019  
+                                <i class="fa fa-clock-o"></i> 07.00 Wita
+                            </div>
+                            <div class="event-item">
+                                <h6>Senam Jumat</h6>
+                                <!-- <p>Diharapkan Kepada Seluruh Karyawan/Karywati BWS Sulawesi IV Kendari untuk mengikuti senam jumat : </p> -->
+                                <i class="fa fa-map-marker "></i> Halaman kantor BWS Sulawesi IV Kendari <br>
+                                <i class="fa fa-calendar"></i> Sabtu, 17 Agustus 2019  
+                                <i class="fa fa-clock-o"></i> 07.00 Wita
+                            </div>
+                            <div class="event-item">
+                                <h6>Hari Raya Idul Adha 1440 H</h6>
+                                <!-- <p>Kepada Seluruh Karyawan/Karywati BWS Sulawesi IV Kendari untuk mengambil hewan kurban : </p> -->
+                                <i class="fa fa-map-marker "></i> Parkir Belakang BWS Sulawesi IV Kendari <br>
+                                <i class="fa fa-calendar"></i> Sabtu, 17 Agustus 2019  
+                                <i class="fa fa-clock-o"></i> 07.00 Wita
+                            </div>
                         </div>
+                        <div class="title-container-content"><i class="fa fa-bookmark "></i> Pengunjung Website</div>
+
                     </div>
                     <div class="container-3">
-                        <div class="title-container-content"><i class="fa fa-bullhorn "></i> Poster</div>
                         <div class="poster">
                             <img src="assets/images/banner.jpg">
                         </div>
                     </div>
                 </section>
+
+                <div class="title-container-content"><i class="fa fa-building "></i> Kegiatan Strategis</div>
+                <section class="kegiatan-strategis">
+                    
+                </section>
+
+
                 <footer>
                     <img src="https://www.pu.go.id/assets/images/content/14695bd2f30a48949c97299bc89c7ebf.png">
                 </footer>
